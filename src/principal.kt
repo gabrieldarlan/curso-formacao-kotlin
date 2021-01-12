3eae844a2e9c506ce26ebba01926fdfba72dcd13
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-01-12 11:53:37 -0300

    cap01-completo

--- src/principal.kt
+++ src/principal.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Bem vindo ao bytebank")
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Gabriel"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200.0
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+
+
 }
