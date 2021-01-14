6ddaa461afc6ae1f8e4a48a07d5d7586dbce94e7
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-01-14 10:46:59 -0300

    cap02-04 Utilizando estrutura de repetição

--- src/principal.kt
+++ src/principal.kt
@@ -1,13 +1,41 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Gabriel"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200.0
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
 
+    var i = 0
+    while (i <= 5) {
+        val titular: String = "Gabriel $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
 
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+//    for (i in 5 downTo 1) {
+//        if (i == 4) {
+//            continue
+//        }
+//        val titular: String = "Gabriel $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo: Double = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+    // ou pode ser dessa forma!
+//    for (i in 1.rangeTo(3)){
+//        println(i)
+//    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
