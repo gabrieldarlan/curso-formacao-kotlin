fc9cb43ad7673425a352103175f91af201293f14
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-01-21 10:47:15 -0300

    kurso04 - completo

--- src/br/com/gdarlan/bytebank/main.kt
+++ src/br/com/gdarlan/bytebank/main.kt
@@ -1,36 +1,15 @@
 package br.com.gdarlan.bytebank
 
-import br.com.gdarlan.bytebank.exception.SaldoInsuficienteException
-import testaComportamentosConta
+import br.com.gdarlan.bytebank.modelo.Endereco
+import java.lang.IllegalStateException
 
-//cap02-01
-//Lidando com exceptions com o try expression
 fun main() {
-    println("início main")
-    testaComportamentosConta()
-
-    println("fim main")
-}
-
-
-fun funcao1() {
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: SaldoInsuficienteException) {
-        e.printStackTrace()
-        println("SaldoInsuficienteException capturado")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua nena")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento é obrigatório")
+        println(tamanhoComplemento)
     }
-    println("fim funcao1")
-}
 
-fun funcao2() {
-    println("início funcao2")
-//teste
-    for (i in 1..5) {
-        println(i)
-        val endereco = Any()
-        throw SaldoInsuficienteException("Saldo insuficiente")
-    }
-    println("fim funcao2")
 }
---
+++ src/br/com/gdarlan/bytebank/teste/testaPilha.kt
@@ -0,0 +1,32 @@
+package br.com.gdarlan.bytebank.teste
+
+import br.com.gdarlan.bytebank.exception.SaldoInsuficienteException
+
+//fun main() {
+//    println("início main")
+//
+//    println("fim main")
+//}
+//
+//
+//fun funcao1() {
+//    println("início funcao1")
+//    try {
+//        funcao2()
+//    } catch (e: SaldoInsuficienteException) {
+//        e.printStackTrace()
+//        println("SaldoInsuficienteException capturado")
+//    }
+//    println("fim funcao1")
+//}
+//
+//fun funcao2() {
+//    println("início funcao2")
+////teste
+//    for (i in 1..5) {
+//        println(i)
+//        val endereco = Any()
+//        throw SaldoInsuficienteException("Saldo insuficiente")
+//    }
+//    println("fim funcao2")
+//}
